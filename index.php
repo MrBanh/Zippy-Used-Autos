26d05fb33db60eb743824120cb789c4dd6bd5bd4
MrBanh <tonybanh@live.com>
2021-03-10 05:51:05 -0800

    Feat: Update controller to handle filters

--- index.php
+++ index.php
@@ -4,6 +4,9 @@ require_once('util/util.php');
 
 // Model - table php files
 require_once('model/vehicles_table.php');
+require_once('model/makes_table.php');
+require_once('model/types_table.php');
+require_once('model/classes_table.php');
 
 $action = filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING);
 if (!$action) {
@@ -15,7 +18,40 @@ if (!$action) {
 
 switch ($action) {
     case 'vehicles_list':
-        $vehicles_list = get_vehicles();
+        $make_id = filter_input(INPUT_GET, 'make_id', FILTER_VALIDATE_INT);
+        $type_id = filter_input(INPUT_GET, 'type_id', FILTER_VALIDATE_INT);
+        $class_id = filter_input(INPUT_GET, 'class_id', FILTER_VALIDATE_INT);
+        $sort_by = filter_input(INPUT_GET, 'sort_by', FILTER_SANITIZE_STRING) ?? 'price';
+
+        if ($make_id || $type_id || $class_id || $sort_by) {
+            $filters = [];
+            if ($make_id) {
+                $filters['make_id'] = $make_id;
+            }
+
+            if ($type_id) {
+                $filters['type_id'] = $type_id;
+            }
+
+            if ($class_id) {
+                $filters['class_id'] = $class_id;
+            }
+
+            $vehicles_list = get_vehicles_filtered($sort_by, $filters);
+        } else {
+            $vehicles_list = get_vehicles();
+        }
+
+        $makes_list = get_makes();
+        $types_list = get_types();
+        $classes_list = get_classes();
+
+        foreach($vehicles_list as $key => $vehicle) {
+            $vehicles_list[$key]['make_name'] = get_make_name($vehicle['make_id']);
+            $vehicles_list[$key]['type_name'] = get_type_name($vehicle['type_id']);
+            $vehicles_list[$key]['class_name'] = get_class_name($vehicle['class_id']);
+        }
+
         include('view/vehicles_list.php');
         break;
 
