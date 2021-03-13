f942a0c775a09ae529962dce4f2ac4b8a2f40c9b
MrBanh <tonybanh@live.com>
2021-03-13 03:38:09 -0800

    Update footer to display links based on action

--- view/admin_footer.php
+++ view/admin_footer.php
@@ -1,14 +1,22 @@
-<!--
-    TODO: Create links to:
-                - View Full Vehicle List (if filtered or on another admin page)
-                - Click here to add a vehicle (if not on add_vehicle_form)
-                - View/Edit Vehicle Makes (if not on view_edit_makes_form)
-                - View/Edit Vehicle Types (if not on view_edit_types_form)
-                - View/Edit Vehicle Classes (if not on view_edit_classes_form)
- -->
  <div class="links">
+    <?php if ($action != 'vehicles_list') { ?>
+        <p><a href="?action=vehicles_list">View Full Vehicle List</a></p>
+    <?php } ?>
+
     <?php if ($action != 'add_vehicle_form') { ?>
         <p><a href="?action=add_vehicle_form">Click here</a> to add a vehicle</p>
+    <?php } ?>
+
+    <?php if ($action != 'makes_list') { ?>
+        <p><a href="?action=makes_list">View/Edit Vehicle Makes</a></p>
+    <?php } ?>
+
+    <?php if ($action != 'types_list') { ?>
+        <p><a href="?action=types_list">View/Edit Vehicle Types</a></p>
+    <?php } ?>
+
+    <?php if ($action != 'classes_list') { ?>
+        <p><a href="?action=classes_list">View/Edit Vehicle Classes</a></p>
     <?php } ?>
  </div>
 </main>
