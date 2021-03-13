9a04498170ed26bf3b06e7561354f741548c1997
MrBanh <tonybanh@live.com>
2021-03-13 03:36:55 -0800

    Feat: Update classes model - Add class to database function - Delete class from database function

--- model/classes_table.php
+++ model/classes_table.php
@@ -28,4 +28,41 @@
         return $class['class_name'] ?? null;
     }
 
+    /**
+     * Add a new vehicle class to the classes table in the database
+     * @param { string } $class_name - Vehicle class name
+     * @return { int } $count - Row affected in database
+     */
+    function add_class($class_name) {
+        global $db;
+        $count = 0;
+        $query = "INSERT INTO classes (class_name)
+                    VALUES (:class_name)";
+        $statement = $db->prepare($query);
+        $statement->bindValue(':class_name', $class_name);
+        if ($statement->execute()) {
+            $count = $statement->rowCount();
+        }
+        $statement->closeCursor();
+        return $count;
+    }
+
+    /**
+     * Deletes a vehicle class from the classes table in the db
+     * @param { int } $class_id - Vehicle class id
+     * @return { int } $count - Row affected in database
+     */
+    function delete_class($class_id) {
+        global $db;
+        $count = 0;
+        $query = "DELETE FROM classes
+                    WHERE class_id = :class_id";
+        $statement = $db->prepare($query);
+        $statement->bindValue(':class_id', $class_id);
+        if ($statement->execute()) {
+            $count = $statement->rowCount();
+        }
+        $statement->closeCursor();
+        return $count;
+    }
 ?>
