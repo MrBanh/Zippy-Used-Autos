a2924b0eea4b779f05b341e48b04efbc66e26f14
MrBanh <tonybanh@live.com>
2021-03-21 21:48:15 -0700

    Update header.php to register/signout links

--- view/header.php
+++ view/header.php
@@ -14,5 +14,20 @@
     <title>Zippy Used Autos</title>
 </head>
 <body class="p-4">
-    <header class="mb-2 border-bottom border-1 border-secondary"><h1>Zippy Used Autos</h1></header>
+    <header class="mb-2 border-bottom border-1 border-secondary d-flex flex-column flex-md-row-reverse justify-content-md-between align-items-md-center">
+        <?php if(!isset($_SESSION['userid']) &&
+                ($action != 'register' && $action != 'logout')) { ?>
+
+            <p class="align-self-end"><a href="?action=register" class="fw-bold">Register</a></p>
+
+        <?php } elseif (isset($_SESSION['userid']) &&
+                        ($action != 'register' && $action != 'logout')) { ?>
+
+            <p class="align-self-end"><?= "Welcome {$_SESSION['userid']}! "?>(<a href="?action=logout" class="fw-bold">Sign Out</a>)</p>
+
+        <?php } else { ?>
+            <div></div>
+        <?php } ?>
+        <h1>Zippy Used Autos</h1>
+    </header>
    <main>
