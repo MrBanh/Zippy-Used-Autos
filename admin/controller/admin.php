281172f7f523ec0ff877a6d83ef5026dfe942d1c
MrBanh <tonybanh@live.com>
2021-04-02 20:52:35 -0700

    Added admin controller to handle actions/route
    - login
    - register
    - logout
    - show_login
    - show_register
    
    Add admin controller
    - TODO: Handle routes/actions for admin
    
    Added route/action handling in admin controller

---
+++ admin/controller/admin.php
@@ -0,0 +1,54 @@
+<?php
+switch ($action) {
+    case 'login': {
+        $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
+        $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+        if (is_valid_admin_login($username, $password)) {
+            $_SESSION['is_valid_admin'] = true;
+            header("Location: .?action=vehicles_list");
+        } else {
+            $login_message = 'You mut login to view this page.';
+            include('../view/login.php');
+        }
+        break;
+    }
+
+    case 'show_login': {
+        include('../view/login.php');
+        break;
+    }
+
+    case 'register': {
+        $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
+        $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+        $confirm_password = filter_input(INPUT_POST, 'confirm_password', FILTER_SANITIZE_STRING);
+
+        if ($username && $password && $confirm_password) {
+            include('../util/valid_register.php');
+            $errors = valid_registration($username, $password, $confirm_password);
+
+            if (count($errors)) {
+                include('../view/register.php');
+            } else {
+                add_admin($username, $password);
+                $_SESSION['is_valid_admin'] = true;
+                header("Location: .?action=vehicles_list");
+            }
+        }
+        break;
+    }
+
+    case 'show_register': {
+        include('../view/register.php');
+        break;
+    }
+
+    case 'logout': {
+        $_SESSION = array();        // Clear session data
+        session_destroy();      // Clean up session id
+        $login_message = 'You have been logged out.';
+        include('../view/login.php');
+        break;
+    }
+}
+?>
